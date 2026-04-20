1246e17b632a3d370b228a444e4c6ef378bf0a59
rabbitkg <rabbitkgbd@gmail.com>
2026-04-21 03:56:11 +0600

    Revalidation after server action to display form data.

--- src/lib/actions.js
+++ src/lib/actions.js
@@ -1,7 +1,27 @@
+import { revalidatePath } from "next/cache";
+import { postTask } from "./tasks";
+
 export const createATask = async (formData) => {
 'use server';
-const name = formData.get('name');
+// const name = formData.get('name');
+// const title = formData.get('title');
+// const description = formData.get('description');
+// const priority = formData.get('priority');
+// const status = formData.get('status');
+// const assignedTo = formData.get('assignedTo');
 
-console.log('Adding a task with name:', name);
+
+// const newTask = {title, description, priority, status, assignedTo};
+
+
+const newTask = Object.fromEntries(formData.entries());
+
+console.log('Adding a task with name:', newTask);
+
+const res = await postTask(newTask);
+if (res.ok) {
+    revalidatePath('/tasks');
+}
+return res;
 
 }
--- src/lib/tasks.js
+++ src/lib/tasks.js
@@ -5,3 +5,13 @@ import tasks from "../data/tasks.json";
 export const getTasks = async () => {
     return tasks
 }
+
+export const postTask = async (newTask) => {
+    newTask.id = tasks.length + 1;
+    tasks.push(newTask);
+    return {
+        ok: true,
+        message: 'Task added successfully',
+        // task: newTask
+    }
+}
